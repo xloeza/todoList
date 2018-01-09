ec53c9951b6c559c4cdbf0fc42b54542f4f03d41
Xavier Loeza <xloeza@nearsoft.com>
2018-01-08 21:12:25 -0700

    This is a todo start app

---
+++ todo-server/routes/users.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.json([{
+  	id: 1,
+  	username: "xloeza"
+  }, {
+  	id: 2,
+  	username: "yloeza"
+  }]);
+});
+
+module.exports = router;
